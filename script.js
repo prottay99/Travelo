1d21fbb392ec4f2aa4befbc7ca5e40e83a344056
prottay <prottay.saha46@gmail.com>
2025-02-17 01:06:29 +0600

    upload full travel website design

---
+++ script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.nav-links')
+function openSidebar() {
+    navbar.classList.add('show')
+}
+function closeSidebar() {
+    navbar.classList.remove('show')
+}
+
+
